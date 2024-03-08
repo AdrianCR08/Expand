cdf22eb639ff69c4f021ee2d6e0cece25f174202
AdrianCR <adrianrmz917@gmail.com>
2024-03-08 17:22:12 -0600

    Initial commit Files added

---
+++ script.js
@@ -0,0 +1,4 @@
+function toggleColumns() {
+  var nestedColumns = document.getElementById("nested-columns");
+  nestedColumns.style.display = (nestedColumns.style.display === "none") ? "block" : "none";
+}
